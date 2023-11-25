49b06bb8d27d54ddd3be5418c9e8b1f88361cc16
kamiyar.k7 <kami.kh0038@gmail.com>
2023-11-25 19:03:02 +0330

    created Api

--- EndPoint/Controllers/ProductController.cs
+++ EndPoint/Controllers/ProductController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MediatR;
+using Application.Products.Queries.GetAllActiveProducts;
 
 namespace EndPoint.Controllers;
 
 public class ProductController : Controller
 {
+	private readonly IMediator _mediator;
+
+	public ProductController(IMediator mediator) 
+	{
+
+		_mediator  = mediator;
+	}
+
+
 	public IActionResult Index()
 	{
-		return View();
+		GetAllActiveProductsQuery query = new GetAllActiveProductsQuery();
+		var respnse = _mediator.Send(query);
+		return View(respnse);
 	}
 }
---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Products;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Application.Products.Queries.GetAllActiveProducts;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : Controller
+{
+	private readonly IMediator _mediator;
+
+	public ProductController(IMediator mediator) 
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet]
+	public async Task< List<Product>> GetProducts(int pagenumber ) 
+	{
+		GetAllActiveProductsQuery query = new GetAllActiveProductsQuery() { PageNumber = pagenumber };
+		var response = await _mediator.Send(query);
+
+		return response;
+	}
+
+	
+}
